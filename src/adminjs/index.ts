30f3b5599d4dcff9540d2ad6324352c8800373a0
Carlos Eduardo <cadumouralemes@gmail.com>
2025-05-12 16:11:27 -0300

    Adding translate

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -5,6 +5,7 @@ import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
 import { User } from "../models/User";
 import bcrypt from 'bcrypt'
+import { locale } from "./locale";
 
 AdminJS.registerAdapter(AdminJSSEquelize)
 
@@ -12,6 +13,7 @@ export const adminJs = new AdminJS({
     databases: [sequelize],
     rootPath: "/admin",
     resources: adminJsResources,
+    locale: locale,
     branding: {
         companyName: 'OneBitFlix',
         logo: '/logoOneBitFlix.svg',
