ce90a4cff073f4ea3935aa536fa3a443deb9473e
Carlos Eduardo <cadumouralemes@gmail.com>
2025-05-12 22:05:15 -0300

    Improving dashboard

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -6,6 +6,7 @@ import { adminJsResources } from "./resources";
 import { User } from "../models/User";
 import bcrypt from 'bcrypt'
 import { locale } from "./locale";
+import { Category, Course, Episode } from "../models";
 
 AdminJS.registerAdapter(AdminJSSEquelize)
 
@@ -34,6 +35,22 @@ export const adminJs = new AdminJS({
             hoverBg: '#151515',
           }
         }
+    },
+    dashboard: {
+      component: AdminJS.bundle('./components/Dashboard'),
+      handler: async (requestAnimationFrame, res, context) => {
+        const course = await Course.count()
+        const episodes = await Episode.count()
+        const categories = Category.count()
+        const standardUsers = await User.count({where: { role: 'user'}})
+
+        res.json({
+          'Cursos': course,
+          'Episódios': episodes,
+          'Categorias': categories,
+          'Usuários': standardUsers
+        })
+      }
     }
 })
 
