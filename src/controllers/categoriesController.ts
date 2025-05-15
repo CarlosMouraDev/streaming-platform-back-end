9c22c6c701b32a67065b9aeba7fad2b405827d0c
Carlos Eduardo <cadumouralemes@gmail.com>
2025-05-14 23:11:23 -0300

    categories improvement

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -3,6 +3,18 @@ import { Category } from "../models"
 
 export const categoriesController = {
     index: async (req: Request, res: Response) => {
+        const { page, perPage } = req.query
+
+        const perPageNumber = typeof perPage === 'string' && parseInt(perPage, 10) > 0
+            ? parseInt(perPage, 10)
+            : 10
+
+        const pageNumber = typeof page === 'string' && parseInt(page, 10) > 0
+            ? parseInt(page, 10)
+            : 1
+
+		const offset = (pageNumber - 1) * perPageNumber
+
         try {
         const categories = await Category.findAll({
             attributes: ['id', 'name', 'position'],
@@ -14,6 +26,6 @@ export const categoriesController = {
             if (err instanceof Error) {
                 return res.status(400).json({ message: err.message })
             }
-    }
+        }
     } 
 }
