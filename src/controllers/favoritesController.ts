09b8ba8939e82bf1054051bb0a34cce9ae1b1c03
Carlos Eduardo <cadumouralemes@gmail.com>
2025-05-22 17:52:04 -0300

    deleting a favorite

--- src/controllers/favoritesController.ts
+++ src/controllers/favoritesController.ts
@@ -1,6 +1,7 @@
 import { Response } from 'express'
 import { AuthenticatedRequest } from "../middlewares/auth";
 import { favoriteService } from '../services/favoriteService'
+import { Favorite } from '../models';
 
 export const favoritesController = {
   //GET /favorites
@@ -24,6 +25,21 @@ export const favoritesController = {
     try {
       const favorite = await favoriteService.create(userId, Number(courseId))
       return res.status(201).json(favorite)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+  // DELETE /favorites/:id
+  delete: async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user!.id
+
+    const courseId = req.params.id
+
+    try {
+      await favoriteService.delete(userId, Number(courseId))
+      return res.status(204).send()
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message })
