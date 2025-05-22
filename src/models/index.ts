6c6baa996c9d8596c050c77a8f9b4dbb39077764
Carlos Eduardo <cadumouralemes@gmail.com>
2025-05-21 21:54:55 -0300

    Adding favorites

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,19 +1,28 @@
-import { Category } from "./Category";
-import { Course } from "./Course";
-import { Episode } from "./Episode";
+import { Category } from './Category'
+import { Course } from './Course'
+import { Episode } from './Episode'
+import { Favorite } from './Favorite'
+import { User } from './User'
 
-Category.hasMany(Course, {
-    as: 'courses'
-})
-Course.hasMany(Episode, { as: 'episodes' })
+Category.hasMany(Course, { as: 'courses' })
 
 Course.belongsTo(Category)
-Course.hasMany(Episode)
+Course.hasMany(Episode, { as: 'episodes'})
+Course.belongsToMany(User, { through: Favorite })
+Course.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'course_id' })
 
 Episode.belongsTo(Course)
+
+Favorite.belongsTo(Course)
+Favorite.belongsTo(User)
+
+User.belongsToMany(Course, { through: Favorite })
+User.hasMany(Favorite, { as: 'favoritesCourses', foreignKey: 'user_id' })
 
 export {
     Category,
     Course,
-    Episode
+    Episode,
+    Favorite,
+    User
 }
