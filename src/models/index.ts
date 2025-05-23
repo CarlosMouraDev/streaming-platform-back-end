8144f5030261b243b7638acdfb4b0b8979ebca23
Carlos Eduardo <cadumouralemes@gmail.com>
2025-05-22 23:30:57 -0300

    Adding Likes table

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,11 +2,13 @@ import { Category } from './Category'
 import { Course } from './Course'
 import { Episode } from './Episode'
 import { Favorite } from './Favorite'
+import { Like } from './Like'
 import { User } from './User'
 
 Category.hasMany(Course, { as: 'courses' })
 
 Course.belongsTo(Category)
+Course.belongsToMany(User, {through: Like})
 Course.hasMany(Episode, { as: 'episodes'})
 Course.belongsToMany(User, { through: Favorite })
 Course.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'course_id' })
@@ -18,11 +20,13 @@ Favorite.belongsTo(User)
 
 User.belongsToMany(Course, { through: Favorite })
 User.hasMany(Favorite, { as: 'favoritesCourses', foreignKey: 'user_id' })
+User.belongsToMany(Course, { through: Like })
 
 export {
-    Category,
-    Course,
-    Episode,
-    Favorite,
-    User
+  Category,
+  Course,
+  Episode,
+  Favorite,
+  Like,
+  User
 }
