bba606399e3aaeca12d64776fbb5f4e0bf55d675
Carlos Eduardo <cadumouralemes@gmail.com>
2025-05-27 23:55:46 -0300

    Adding episode progres

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,6 +4,7 @@ import { Episode } from './Episode'
 import { Favorite } from './Favorite'
 import { Like } from './Like'
 import { User } from './User'
+import { WatchTime } from './WatchTime'
 
 Category.hasMany(Course, { as: 'courses' })
 
@@ -14,11 +15,13 @@ Course.belongsToMany(User, { through: Favorite })
 Course.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'course_id' })
 
 Episode.belongsTo(Course)
+Episode.belongsToMany(User, { through: WatchTime })
 
 Favorite.belongsTo(Course)
 Favorite.belongsTo(User)
 
 User.belongsToMany(Course, { through: Favorite })
+User.belongsToMany(Episode, { through: WatchTime })
 User.hasMany(Favorite, { as: 'favoritesCourses', foreignKey: 'user_id' })
 User.belongsToMany(Course, { through: Like })
 
@@ -27,6 +30,6 @@ export {
   Course,
   Episode,
   Favorite,
-  Like,
-  User
+  User,
+	WatchTime
 }
--- src/services/episodeService.ts
+++ src/services/episodeService.ts
@@ -1,13 +1,12 @@
 import { Response } from "express";
 import path from "path";
 import fs from 'fs'
+import { WatchTime, WatchTimeAttributes } from "../models/WatchTime";
 
 export const episodeService = {
     streamEpisodeToResponse: (res: Response, videoUrl: string, range: string | undefined) => {
          const filePath = path.join(__dirname, '..', '..', 'uploads', videoUrl)
             const fileStat = fs.statSync(filePath)
-
-            
 
             if (range) {
                 const parts = range.replace(/bytes=/, '').split('-')
@@ -39,5 +38,39 @@ export const episodeService = {
 
                 fs.createReadStream(filePath).pipe(res)
             }
+    },
+
+    getWatchTime: async (userId: number, episodeId: number) => {
+        const watchTime = await WatchTime.findOne({
+            attributes: ['seconds'],
+            where: {
+                userId,
+                episodeId
+            }
+        })
+    },
+
+    setWatchTime: async ({ userId, episodeId, seconds }: WatchTimeAttributes) => {
+        const watchTimeAlreadyExists = await WatchTime.findOne({
+            where: {
+                userId,
+                episodeId
+            }
+        })
+
+        if (watchTimeAlreadyExists) {
+            watchTimeAlreadyExists.seconds = seconds
+            await watchTimeAlreadyExists.save()
+
+            return watchTimeAlreadyExists
+        } else {
+            const watchTime = await WatchTime.create({
+                userId,
+                episodeId,
+                seconds
+            })
+    
+            return watchTime
+        }
     }
 }
